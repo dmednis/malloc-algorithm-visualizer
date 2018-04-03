2c0454ddcb9a02235ac98271b6b0ac400f02a5f5
Dāvis Mednis <davis.mednis@fullcontact.com>
2018-04-03 21:58:40 +0300

    Add chunks and sizes state structure

--- src/components/Visualization/Visualization.js
+++ src/components/Visualization/Visualization.js
@@ -1,19 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ParameterInputs from "../ParameterInputs";
 import Algorithm from "../Algorithm";
 
 export default class Visualization extends Component {
 
-    render() {
-        return (
-            <div>
-                <ParameterInputs/>
-                <Algorithm name="First fit"/>
-                <Algorithm name="Best fit"/>
-                <Algorithm name="Worst fit"/>
-                <Algorithm name="Buddy's System"/>
-                <Algorithm name="Next fit"/>
-            </div>
-        )
-    }
+  constructor() {
+    super();
+
+    this.state = {
+      chunks: [],
+      sizes: [],
+    };
+  }
+
+  setChunks(rawChunks) {
+    const chunks = rawChunks.map((chunk) => {
+      return {
+        size: chunk,
+        memory: new Array(chunk).fill(0),
+        accessing: false,
+      }
+    });
+    this.setState({ chunks });
+  }
+
+  setSizes(rawSizes) {
+    const sizes = rawSizes.map((size, idx) => {
+      return {
+        id: idx + 1,
+        size: size,
+        allocated: false,
+      }
+    });
+    this.setState({ sizes });
+  }
+
+  render() {
+    const { chunks, sizes } = this.state;
+
+    return (
+      <div>
+        <ParameterInputs setChunks={this.setChunks} setSizes={this.setSizes}/>
+        <Algorithm chunks={chunks} sizes={sizes} name="First fit"/>
+        <Algorithm chunks={chunks} sizes={sizes} name="Best fit"/>
+        <Algorithm chunks={chunks} sizes={sizes} name="Worst fit"/>
+        <Algorithm chunks={chunks} sizes={sizes} name="Buddy's System"/>
+        <Algorithm chunks={chunks} sizes={sizes} name="Next fit"/>
+      </div>
+    )
+  }
 }
