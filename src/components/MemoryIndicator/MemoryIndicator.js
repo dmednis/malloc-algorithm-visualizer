b1de6c52b2da451dc25c1ec514ad41f056c574b2
Elvijs <ev14027@lu.lv>
2018-04-04 00:58:20 +0300

    fixed accessing prop for memBlocks

--- src/components/MemoryIndicator/MemoryIndicator.js
+++ src/components/MemoryIndicator/MemoryIndicator.js
@@ -4,8 +4,8 @@ export default class MemoryIndicator extends Component {
 
 
   render() {
-    const blockHeight = '30px';
-    const blockWidth = '8px';
+    const blockHeight = '40px';
+    const blockWidth = '15px';
     const styles = {
       bigMemStyle: {
         display: 'flex',
@@ -13,7 +13,8 @@ export default class MemoryIndicator extends Component {
         flexWrap: 'wrap',
         border: '1px solid grey',
         maxWidth: '345px',
-        marginTop: '50px',
+        marginTop: '30px',
+        marginBottom: '15px',
         marginLeft: '100px',
       },
       memBlockStyle: {
@@ -33,6 +34,7 @@ export default class MemoryIndicator extends Component {
           height: blockHeight,
           width: blockWidth,
           border: '0.1px solid black',
+          borderBottom: '5px solid blue',
           backgroundColor: 'yellow',
         },
         borderBlock:{
@@ -76,19 +78,15 @@ export default class MemoryIndicator extends Component {
 
     if(chunks){
       chunks.forEach((chunk) =>{
-        if (!chunk.accessing){
           chunk.memory.forEach((mem)=>{
             if(mem > 0){
               memoryBlocks.push(<MemoryBlock status='used'/>)
+            } else if(chunk.accessing) {
+              memoryBlocks.push(<MemoryBlock status='accessing'/>)
             } else {
               memoryBlocks.push(<MemoryBlock status='free'/>)
             }
-          })
-        } else {
-          chunk.memory.forEach(()=>{
-            memoryBlocks.push(<MemoryBlock status='accessing'/>)
           });
-        }
         memoryBlocks.push(<MemoryBlock status='borderBlock'/>)
       });
     }
