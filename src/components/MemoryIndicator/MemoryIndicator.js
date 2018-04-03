468491b8808ea421c1c6818741fa828d7459d96d
Elvijs <ev14027@lu.lv>
2018-04-03 22:43:53 +0300

    small mem block with statuses

--- src/components/MemoryIndicator/MemoryIndicator.js
+++ src/components/MemoryIndicator/MemoryIndicator.js
@@ -7,31 +7,115 @@ export default class MemoryIndicator extends Component {
     const styles = {
       bigMemStyle: {
         display: 'flex',
-        color: 'blue',
-        border: '2px solid red',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        border: '1px solid grey',
+        maxWidth: '220px',
       },
       memBlockStyle: {
-        height: '20px',
-        width: '5px',
-        border: '1px solid green',
+        free:{
+          height: '20px',
+          width: '5px',
+          border: '0.1px solid black',
+          backgroundColor: 'white',
+        },
+        used:{
+          height: '20px',
+          width: '5px',
+          border: '0.1px solid black',
+          backgroundColor: 'green',
+        },
+        accessing: {
+          height: '20px',
+          width: '5px',
+          border: '0.1px solid black',
+          backgroundColor: 'yellow',
+        },
+        borderBlock:{
+          height: '20px',
+          width: '5px',
+          border: '0.1px solid black',
+          backgroundColor: 'grey',
+        },
       }
     };
 
-
-    // //chunks from props
-    // const { chunks } = this.props;
-    //
-    // // chunks = [{
-    // //
-    // // }]
-    let memoryBlocks = [];
-    //
-    // chunks.forEach((chunk) =>{
-    //
-    // });
-    for (let i = 0; i < 105; i += 1) {
-      memoryBlocks.push(<div style={styles.memBlockStyle}></div>)
+    const chunks = [
+      {
+        size: 10,
+        memory: [0,0,0,0,0,0,0,0,0,0],
+        accessing: false,
+      },
+      {
+        size: 20,
+        memory: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        accessing: true,
+      },
+      {
+        size: 15,
+        memory: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        accessing: false,
+      },
+      {
+        size: 25,
+        memory: [0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        accessing: true,
+      },
+      {
+        size: 10,
+        memory: [0,0,0,0,0,1,1,1,0,0],
+        accessing: false,
+      },
+    ];
+    
+    // little mem block
+    function MemoryBlock(props) {
+      let blockStyle;
+      switch (props.status){
+        case 'free': {
+          blockStyle = styles.memBlockStyle.free;
+          break;
+        }
+        case 'used': {
+          blockStyle = styles.memBlockStyle.used;
+          break;
+        }
+        case 'accessing': {
+          blockStyle = styles.memBlockStyle.accessing;
+          break;
+        }
+        case 'borderBlock': {
+          blockStyle = styles.memBlockStyle.borderBlock;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+      return <div style={blockStyle}></div>
     }
+
+    // const { chunks } = this.props;
+
+    let memoryBlocks = [];
+
+    chunks.forEach((chunk) =>{
+      if (!chunk.accessing){
+        chunk.memory.forEach((mem)=>{
+          if(mem > 0){
+            memoryBlocks.push(<MemoryBlock status='used'/>)
+          } else {
+            memoryBlocks.push(<MemoryBlock status='free'/>)
+          }
+        })
+      } else {
+        chunk.memory.forEach(()=>{
+          memoryBlocks.push(<MemoryBlock status='accessing'/>)
+        });
+      }
+      memoryBlocks.push(<MemoryBlock status='borderBlock'/>)
+    });
+
 
     return (
       <div style={styles.bigMemStyle}>
